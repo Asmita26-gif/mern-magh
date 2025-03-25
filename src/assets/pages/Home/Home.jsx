4fcd4174f39d3dc465d8a8df6f3cb5bc3a6b75c2
Asmita Pariyar <pariyarasmita319@gmail.com>
2025-03-24 19:37:51 -0700

    changes

--- src/assets/pages/Home/Home.jsx
+++ src/assets/pages/Home/Home.jsx
@@ -1,21 +1,21 @@
-// import React from 'react'
-// import { FiTool } from 'react-icons/fi'
+import React from 'react'
+import { FiTool } from 'react-icons/fi'
 
-// export default function Home() {
-//   return (
-//     <div className='grid grid-cols-[1fr_1fr] items-center justify-items-center min-h-screen px-6 backdrop-blur-3xl'>
-//       <div className='lg:w-1/2 space-y-6'>
-//         <div className='w-10 h-10 flex items-center justify-center bg-white shadow-md rounded-full'>
-//           <span><FiTool className='mr-2' /></span>
+export default function Home() {
+  return (
+    <div className='grid grid-cols-[1fr_1fr] items-center justify-items-center min-h-screen px-6 backdrop-blur-3xl'>
+      <div className='lg:w-1/2 space-y-6'>
+        <div className='w-10 h-10 flex items-center justify-center bg-white shadow-md rounded-full'>
+          <span><FiTool className='mr-2' /></span>
 
-//         </div>
-//         <h1 className='text-5xl font-bold'>Real time problem</h1>
-//         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit architecto cum magnam, libero, et eaque accusamus, quidem illo corporis sint voluptate dolore aut veniam eius quae? Error, assumenda perspiciatis?</p>
+        </div>
+        <h1 className='text-5xl font-bold'>Real time problem</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit architecto cum magnam, libero, et eaque accusamus, quidem illo corporis sint voluptate dolore aut veniam eius quae? Error, assumenda perspiciatis?</p>
 
-//       </div>
-//     </div >
-//   )
-// }
+      </div>
+    </div >
+  )
+}
 
 
 
